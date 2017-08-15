63993205f5b99d568dbeaa0057364e0d8268d5a5
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-08-15 21:14:34 +0800

    Finish 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add a1, a2
+  a1 + a2
+end
+
+def subtract a1, a2
+  a1 - a2
+end
+
+def sum arr
+  arr.inject(0) {|sum, n| sum + n}
+end
+
+def multiply arr
+  arr.inject(1) {|prod, n| prod * n}
+end
+
+def power base, exp
+  base ** exp
+end
+
+def factorial n
+  f,i = n,1
+
+  f = 1 if n == 0
+
+  for i in 1...n do
+    f = i * f
+  end
+
+  f
+end
