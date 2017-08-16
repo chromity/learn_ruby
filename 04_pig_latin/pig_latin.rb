cb33fbfd34ea3e3c29ed4536a767eb0a24df8a64
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-08-16 15:37:45 +0800

    Used a better solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,26 +1,10 @@
-def translate(param)
-  word_array = param.split(" ")
-  translated = []
+def translate str
+  str.split(" ").map do |word|
+    word =~ /^([^aeiouyq]*(qu)?)(.*)$/x
 
-  word_array.each do |word|
-    if !(word[0] =~ /[aeiou]/i)
-      if !(word[1] =~ /[aeio]/i)
-        if !(word[2] =~ /[aeio]/i)
-          word = word[3...word.length] + word[0..2]
-        elsif word[2] == "u" && word[1] == "q"
-          word = word[3...word.length] + word[0..2]
-        else
-          word = word[2...word.length] + word[0..1]
-        end
-      elsif word[1] == "u" && word[0] == "q"
-          word = word[2..word.length] + word[0..1]
-      else
-        word = word[1...word.length] + word[0]
-      end
-    end
+    first_phoneme = $1
+    rest_of_word = $3
 
-    translated.push(word + "ay")
-  end
-
-  translated.join(" ")
+    "#{rest_of_word}#{first_phoneme}ay"
+  end.join(" ")
 end
