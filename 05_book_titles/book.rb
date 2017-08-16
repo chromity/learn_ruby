1581fbfa06b5b44394637170ec49926c58d04ec1
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-08-16 21:15:09 +0800

    Finish 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title=(title)
+    arr = []
+
+    conjunctions = ["and", "in", "the", "of", "a", "an"]
+
+    title.split.each_with_index do |word, index|
+      if !conjunctions.include?(word) || index == 0
+        word =~ /^(^[\w])(.*)$/x
+        arr.push($1.upcase + $2)
+      else
+        arr.push(word)
+      end
+    end
+
+    @title = arr.join(" ")
+  end
 end
